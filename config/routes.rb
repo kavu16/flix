7cef7df122be0e9cc29a77267a27e83ca8d180c6
Brady Edwards <brady.edwards16@gmail.com>
2023-12-13 13:00:24 -0500

    Modules 26-30 (User model)

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews
   end
+
+  resources :users
+  get "signup" => "users#new"
+
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
 end
